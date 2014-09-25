ef0520f40f55a9f74a91e0b9e84625c5f370868f
robert-fiedor <robert.fiedor@kaplan.com>
2014-09-24 22:09:23 -0400

    2 test passing

--- src/scope.js
+++ src/scope.js
@@ -2,5 +2,21 @@
 'use strict';
 
 function Scope() {
-
+	this.$$watchers=[];
 }
+
+Scope.prototype.$watch = function(watchFn,listenerFn) {
+
+	var watch = {
+		watchFn:watchFn,
+		listenerFn:listenerFn
+	};
+
+	this.$$watchers.push(watch);
+};
+
+Scope.prototype.$digest = function() {
+	_.forEach(this.$$watchers, function(watcher){
+		watcher.listenerFn();
+	});
+};
